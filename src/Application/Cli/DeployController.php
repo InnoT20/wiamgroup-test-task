ff5870352e50021dcdb86158814359c590881f9f
Artem Kotelnikov <artem@innot20.ru>
2023-11-27 11:27:22 +0400

    Fix starting migration before postgres starts

--- src/Application/Cli/DeployController.php
+++ src/Application/Cli/DeployController.php
@@ -5,21 +5,23 @@ declare(strict_types=1);
 namespace App\Application\Cli;
 
 use yii\console\Controller;
+use yii\console\ExitCode;
 
 class DeployController extends Controller
 {
     /**
      * Check connection to postgresql
+     *
      * @return int
      */
     public function actionCheckConnection(): int
     {
         try {
             \Yii::$app->db->open();
-        } catch (\Exception $e) {
-            return 1;
+        } catch (\Throwable) {
+            $this->stdout('No connection');
         }
 
-        return 0;
+        return ExitCode::OK;
     }
 }
